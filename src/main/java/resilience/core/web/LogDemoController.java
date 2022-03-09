1f1b74122945136c6e2e7c410a2dcbb2c895e735
Hayden <soar0706@gmail.com>
2022-03-09 20:27:10 +0900

    9. 빈 스코프

--- src/main/java/resilience/core/web/LogDemoController.java
+++ src/main/java/resilience/core/web/LogDemoController.java
@@ -1,7 +1,6 @@
 package resilience.core.web;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,13 +12,14 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String reqURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
+
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(reqURL);
         myLogger.log("controller test");
         logDemoService.logic("testID");
--- src/main/java/resilience/core/web/LogDemoService.java
+++ src/main/java/resilience/core/web/LogDemoService.java
@@ -1,17 +1,15 @@
 package resilience.core.web;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 import resilience.core.common.MyLogger;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
-    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
+    private final MyLogger myLogger;
 
     public void logic(String id) {
-        MyLogger myLogger = myLoggerObjectProvider.getObject();
         myLogger.log("service test ID = "+id);
     }
 }
